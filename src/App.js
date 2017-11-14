6460d71af4da2f78cb83e0de9a610cfc983fa6d9
Karel Klima <karelklima@gmail.com>
2017-11-14 21:53:16 +0100

    Experiments generator

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Grid
+} from 'material-ui';
+import Experiments from './Experiments';
+import Transformers from './Transformers';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <AppBar position="static">
+            <Toolbar>
+              <Typography type="title" color="inherit">
+                Generators&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </Typography>
+              <Button color="contrast" component={Link} to="/experiments">
+                Experiments
+              </Button>
+              <Button color="contrast" component={Link} to="/transformers">
+                Transformers
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <Route exact path="/">
+            <Redirect to="/experiments"/>
+          </Route>
+          <Route path="/experiments" component={Experiments}/>
+          <Route path="/transformers" component={Transformers}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/Transformers.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Transformers = () => (
+  <div>Transformers</div>
+)
+
+export default Transformers;
